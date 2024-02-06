ff77212e800b5c7dcceb7441bcc5b6b276ae4b1b
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-02-06 17:35:27 -0300

    chore: add aws provider on terraform

---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type = string
+  description = ""
+}
+
+variable "access_key" {
+  type = string
+  description = ""
+}
+
+variable "secret_key" {
+  type = string
+  description = ""
+}
