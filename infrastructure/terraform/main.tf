dd4fa69a8af07a34e99dab94ff28ea4a9f9ce879
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-02-09 12:58:23 -0300

    chore: add aws s3 resource to store terraform remote state

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -8,3 +8,29 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "fingerwar-bucket" {
+  bucket = "tfstate-${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_s3_bucket_versioning" "remote-state" {
+  bucket = aws_s3_bucket.fingerwar-bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_object" "terraform-tfstate" {
+  bucket = aws_s3_bucket_versioning.remote-state.id
+  key    = "infrastructure/terraform/terraform.tfstate"
+  source = "terraform.tfstate"
+}
