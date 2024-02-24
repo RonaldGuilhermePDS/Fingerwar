be39047847e0cdbbac541667ca9e7bd5499f809f
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-02-24 18:59:09 -0300

    chore: duplicate public and private subnets and create db subnet group and db instance

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -20,12 +20,43 @@ resource "aws_vpc" "vpc_dev" {
   enable_dns_hostnames = true
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.vpc_dev.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id            = aws_vpc.vpc_dev.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.vpc_dev.id
-  cidr_block = "10.0.2.0/24"
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id            = aws_vpc.vpc_dev.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id            = aws_vpc.vpc_dev.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+}
+
+
+resource "aws_subnet" "private_subnet_b" {
+  vpc_id            = aws_vpc.vpc_dev.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_db_instance" "db_dev" {
+  allocated_storage    = 10
+  engine               = "postgres"
+  engine_version       = "16.2"
+  instance_class       = "db.t3.micro"
+  username             = var.database_username
+  password             = var.database_password
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.db_dev_subnet.id
+}
+
+resource "aws_db_subnet_group" "db_dev_subnet" {
+  name       = "dbdevsubnet"
+  subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
 }
--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -12,3 +12,13 @@ variable "secret_key" {
   type        = string
   description = ""
 }
+
+variable "database_username" {
+  type = string
+  description = "value"
+}
+
+variable "database_password" {
+  type = string
+  description = "value"
+}
