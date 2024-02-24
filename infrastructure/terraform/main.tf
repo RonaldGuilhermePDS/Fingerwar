728a1ea9b6a38da719304ac5f06dee9709d2908f
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-02-24 16:46:32 -0300

    chore: add aws vpc resource

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -8,3 +8,14 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_vpc" "vpc_dev" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
