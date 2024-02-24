e46fdd0515f77b47e9c62f10d39398cd67684a67
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-02-24 19:33:19 -0300

    chore: add aws nat gateway resource

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -68,3 +68,11 @@ resource "aws_eip" "nat" {
 resource "aws_internet_gateway" "ig" {
   vpc_id = aws_vpc.vpc_dev.id
 }
+
+resource "aws_nat_gateway" "nat_ig" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.private_subnet_a.id
+
+  depends_on = [aws_internet_gateway.ig]
+}
+
